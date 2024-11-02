41f46eb07bddb708940208272cb6b6d70a578917
bill1ben <fcbilel22@gmail.com>
2024-11-02 05:47:12 +0100

    edit URL localhost

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const URL = 'http://127.0.0.1:8000';
-export const API_URL = 'http://127.0.0.1:8000/api';
-export const MEDIA_URL = 'http://127.0.0.1:8000/uploads/media';
+export const URL = 'http://localhost'
+export const API_URL = 'http://localhost:8000/api'
+export const MEDIA_URL = 'http://localhost:8000/uploads/media'
